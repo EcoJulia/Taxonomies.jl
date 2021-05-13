72f26a41b93321ac3ba108d30e55ab083dd04e5d
michael catchen <michael.catchen@colorado.edu>
2021-05-13 15:40:58 -0400

    adding deps

--- src/BioTIME.jl
+++ src/BioTIME.jl
@@ -1,5 +1,18 @@
 module BioTIME
 
-greet() = print("Hello World!")
+using TimeSeries
+using DataFrames
+using NCBITaxonomy
+
+if !haskey(ENV, "BIOTIME_PATH")
+    @warn """
+    The environmental variabe BIOTIME_PATH is not set, so the data will
+    be stored in the package path. This is not ideal, and you really should set
+    the BIOTIME_PATH.
+    """
+end
+const taxpath = get(ENV, "BIOTIME_PATH", joinpath(@__DIR__, "..", "deps"))
+ispath(taxpath) || mkpath(taxpath)
+
 
 end # module
